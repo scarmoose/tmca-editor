76d96723118e4368c6709a057e2563f4a6fbb6a3
Christoffer <Christoffer@DESKTOP-9NH7Q1U>
2016-10-25 09:09:54 +0200

    DeleteCommand

--- tmca-uml/Commands/Edit/DeleteCommand.cs
+++ tmca-uml/Commands/Edit/DeleteCommand.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace tmca_uml.Commands.Edit
 {
-    class DeleteCommand
+    public class DeleteCommand : ICommand
     {
+        public string Header { get; set; }
+        public KeyGesture Gesture { get; set; }
+        public event EventHandler CanExecuteChanged;
+
+        public DeleteCommand()
+        {
+            Header = "Delete";
+
+            Gesture = new KeyGesture(Key.Delete, ModifierKeys.Control);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Deleted");
+        }
+  
+        public string GestureText
+        {
+            get { return Gesture.GetDisplayStringForCulture(CultureInfo.CurrentUICulture); }
+        }
     }
 }
