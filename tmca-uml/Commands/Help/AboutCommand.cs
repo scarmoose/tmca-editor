f4792d1ba1a3d6737b338688a4c9aa2ccc2ec716
Anders TT <s140996@student.dtu.dk>
2016-10-25 11:08:15 +0200

    Help

---
+++ tmca-uml/Commands/Help/AboutCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace tmca_uml.Commands.Help
+{
+    public class AboutCommand : ICommand
+    {
+        public string Header { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public AboutCommand()
+        {
+            Header = "About";
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var mainWindow = (Application.Current.MainWindow as MainWindow);
+            if (mainWindow != null)
+            {
+                MessageBox.Show("About info...");
+                mainWindow.data.StatusBarLabel = "About was showned succesfully...";
+            }
+            else
+            {
+                mainWindow.data.StatusBarLabel = "Show About failed!";
+            }
+        }
+    }
+}
---
+++ tmca-uml/Commands/Help/HelpCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace tmca_uml.Commands.Help
+{
+    public class HelpCommand : ICommand
+    {
+        public string Header { get; set; }
+
+        public KeyGesture Gesture { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public HelpCommand()
+        {
+            Header = "Help";
+
+            Gesture = new KeyGesture(Key.F1, ModifierKeys.Control);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var mainWindow = (Application.Current.MainWindow as MainWindow);
+            if (mainWindow != null)
+            {
+                MessageBox.Show("Help info...");
+                mainWindow.data.StatusBarLabel = "Help was shownes succesfully...";
+            }
+            else
+            {
+                mainWindow.data.StatusBarLabel = "Show Help failed!";
+            }
+        }
+
+        public string GestureText
+        {
+            get { return Gesture.GetDisplayStringForCulture(CultureInfo.CurrentUICulture); }
+        }
+    }
+}
