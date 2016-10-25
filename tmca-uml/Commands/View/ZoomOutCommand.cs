9f93c150d914e4b57b6713fedd0d75a697b8a366
Christoffer <Christoffer@DESKTOP-9NH7Q1U>
2016-10-25 09:26:35 +0200

    ZoomOutCommand

--- tmca-uml/Commands/View/ZoomOutCommand.cs
+++ tmca-uml/Commands/View/ZoomOutCommand.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace tmca_uml.Commands.View
 {
-    class ZoomOutCommand
+    public class ZoomOutCommand : ICommand
     {
+        public string Header { get; set; }
+        public event EventHandler CanExecuteChanged;
+
+        public ZoomOutCommand()
+        {
+            Header = "Zoom Out";
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Zoom out");
+        }
     }
 }
