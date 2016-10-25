4fa355e7c7a185efd0a50496c778cf219845cdb5
Anders TT <s140996@student.dtu.dk>
2016-10-25 09:14:23 +0200

    Paste Command

--- tmca-uml/Commands/Edit/PasteCommand.cs
+++ tmca-uml/Commands/Edit/PasteCommand.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace tmca_uml.Commands.Edit
 {
-    class PasteCommand
+    public class PasteCommand : ICommand
     {
+        public string Header { get; set; }
+
+        public KeyGesture Gesture { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public PasteCommand()
+        {
+            Header = "Copy";
+
+            Gesture = new KeyGesture(Key.V, ModifierKeys.Control);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Copy something!");
+        }
+
+        public string GestureText
+        {
+            get { return Gesture.GetDisplayStringForCulture(CultureInfo.CurrentUICulture); }
+        }
     }
 }
