50694afde716b6db6fe6ab8511cc45a546876674
Christoffer <Christoffer@DESKTOP-9NH7Q1U>
2016-10-25 08:57:08 +0200

    SaveCommand

--- tmca-uml/Commands/File/SaveCommand.cs
+++ tmca-uml/Commands/File/SaveCommand.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace tmca_uml.Commands
 {
-    class SaveCommand
+    public class SaveCommand : ICommand
     {
+        public string Header { get; set; }
+        public KeyGesture Gesture { get; set; }
+        public event EventHandler CanExecuteChanged;
+
+        public SaveCommand()
+        {
+            Header = "Save";
+
+            Gesture = new KeyGesture(Key.S, ModifierKeys.Control);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Save succesfull");
+        }
+
+        public string GestureText
+        {
+            get { return Gesture.GetDisplayStringForCulture(CultureInfo.CurrentUICulture); }
+        }
     }
 }
