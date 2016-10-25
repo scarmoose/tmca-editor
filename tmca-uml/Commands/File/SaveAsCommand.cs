3ce0c00a5b80e878c51da491ef2521da9fad819c
Anders TT <s140996@student.dtu.dk>
2016-10-25 08:59:54 +0200

    Save As command

--- tmca-uml/Commands/File/SaveAsCommand.cs
+++ tmca-uml/Commands/File/SaveAsCommand.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace tmca_uml.Commands
 {
-    class SaveAsCommand
+    public class SaveAsCommand : ICommand
     {
+        public string Header { get; set; }
+
+
+        public event EventHandler CanExecuteChanged;
+
+        public SaveAsCommand()
+        {
+            Header = "Save As";
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Save As something!"); ;
+        }
     }
 }
