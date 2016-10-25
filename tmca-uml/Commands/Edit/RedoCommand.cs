de6a57eed5a0e82888032bb72c446c8582ba901e
Christoffer <Christoffer@DESKTOP-9NH7Q1U>
2016-10-25 09:14:27 +0200

    RedoCommand

--- tmca-uml/Commands/Edit/RedoCommand.cs
+++ tmca-uml/Commands/Edit/RedoCommand.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace tmca_uml.Commands.Edit
 {
-    class RedoCommand
+    public class RedoCommand : ICommand
     {
+        public string Header { get; set; }
+        public KeyGesture Gesture { get; set; }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Redo");
+        }
+
+        public string GestureText
+        {
+            get { return Gesture.GetDisplayStringForCulture(CultureInfo.CurrentUICulture); }
+        }
     }
 }
